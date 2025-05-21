94c3222a8474ee9c1c69ae8b66971f3ee4a98ef7
Maxson Almeida <maxsonferovante@gmail.com>
2025-05-21 08:09:36 -0300

    initialize search service project with Gradle and Spring Boot

---
+++ src/test/java/com/maal/searchservice/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maal.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
